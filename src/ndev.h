e2864c44438ca3cbb3ce48937617effa35415a86
jj-cwc <jeff@coldwarcomputing.com>
2022-03-23 21:44:22 -0500

    Added networking functions

--- src/ndev.h
+++ src/ndev.h
@@ -11,5 +11,7 @@ void init_sio();
 uint8_t nstatus(nstatus_t *status);
 uint8_t nopen(char *filespec, uint8_t mode, uint8_t translation);
 uint8_t nclose();
+uint8_t nread(char *buf, uint16_t len);
+uint8_t nwrite(char *buf, uint16_t len);
 
 #endif
